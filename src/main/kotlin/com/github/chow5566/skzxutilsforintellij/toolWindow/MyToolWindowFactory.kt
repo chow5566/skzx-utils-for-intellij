4a8ffb2416b8fda869b85f8379aea7c32d6a3070
GitHub Action <action@github.com>
2025-07-03 02:01:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/chow5566/skzxutilsforintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/chow5566/skzxutilsforintellij/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.chow5566.skzxutilsforintellij.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.chow5566.skzxutilsforintellij.MyBundle
+import com.github.chow5566.skzxutilsforintellij.services.MyProjectService
 import javax.swing.JButton
 
 
